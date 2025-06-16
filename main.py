6883617072a0a43d98cfc4d209ca14938d925ee8
nastya-exe <lena.krytko@mail.ru>
2025-06-16 15:28:45 +0300

    Добавила новые изменения

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
+from fastapi.responses import HTMLResponse
+
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
 
 # Чтобы фронт (если будет с другого порта) мог обращаться
 app.add_middleware(
@@ -18,10 +21,9 @@ app.add_middleware(
 # Раздаём статичные файлы (html, js, css)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/")
-async def root():
-    content = {"message": "Привет, Настя! FastAPI работает."}
-    return JSONResponse(content=content, media_type="application/json; charset=utf-8")
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 # Пример простого расписания на неделю (по дням)
 @app.get("/api/schedule")
