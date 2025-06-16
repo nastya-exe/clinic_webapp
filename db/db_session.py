a28d4541aedc7942099c56e2f3cf2f3eb04211d0
nastya-exe <lena.krytko@mail.ru>
2025-06-16 19:04:51 +0300

    Добавила новые изменения

--- db/db_session.py
+++ db/db_session.py
@@ -1,10 +1,11 @@
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+sys.path.append(os.path.dirname(os.path.abspath(os.path.join(__file__, '..', '..'))))
+
+from config import DATABASE_URL
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import declarative_base
-from config import DATABASE_URL
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
