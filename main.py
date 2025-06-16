5bee8ae9c59c8f7dd3911a86fc13c19fbbab67e5
nastya-exe <lena.krytko@mail.ru>
2025-06-16 14:36:11 +0300

    Добавила новые изменения

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
-
 
 app = FastAPI()
 
@@ -10,9 +8,6 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
-
-@app.get("/", response_class=HTMLResponse)
-async def get_calendar(request: Request):
+@app.get("/")
+async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
-
-
